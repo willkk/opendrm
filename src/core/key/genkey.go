3a1d3662289e7240a61cf616af06daea772f71c2
maxxcwu <maxxcwu@tencent.com>
2018-11-20 20:27:22 +0800

    update license notice in genkey.go

--- src/core/key/genkey.go
+++ src/core/key/genkey.go
@@ -1,8 +1,21 @@
-//	Opendrm, an open source implementation of industry-grade DRM(Digital Rights Management) or Key System.
-//  Copyright (C) 2018  wilkk
-//	This file is part of opendrm Project. It is subject to the license terms in the LICENSE file found in
-// 	the top-level directory of this distribution. No part of Foo Project, including this file, may be copied,
-// 	modified, propagated, or distributed except according to the terms contained in the LICENSE file.
+/*
+	Opendrm, an open source implementation of industry-grade DRM
+	(Digital Rights Management) or Key System.
+	Copyright (C) 2018  wilkk
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 /*
 	This file can be used to generate encryption&decryption key. You can use the
